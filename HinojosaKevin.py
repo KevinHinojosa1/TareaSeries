bb0b8c0f020002d16388ca8736bdafdfea9e5abe
“KevinHinojosa1” <steven_hinojosa@hotmail.com>
2025-01-26 00:09:01 -0500

    NombreApellido

---
+++ HinojosaKevin.py
@@ -0,0 +1,23 @@
+# Información Personal
+# Autor: Kevin Hinojosa
+# Fecha: 26/01/2025
+
+# Definición de variables
+nombre = "Kevin Hinojosa"
+cedula = "1719734228"
+fecha_nacimiento = "05/10/1997"
+edad = 26
+instituto = "Instituto Japón"
+carrera = "Inteligencia Artificial"
+
+# Impresión de la información
+print("=" * 50)
+print("        INFORMACIÓN DEL ESTUDIANTE")
+print("=" * 50)
+print(f"Nombre: {nombre}")
+print(f"Número de Cédula: {cedula}")
+print(f"Fecha de Nacimiento: {fecha_nacimiento}")
+print(f"Edad: {edad} años")
+print(f"Estudiante de {carrera}")
+print(f"Instituto: {instituto}")
+print("=" * 50)
